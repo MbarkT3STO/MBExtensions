e7a9eaf0d3f46bf860b21700326d700aa2cfdf92
MBARK T3STO <mbarktiesto@outlook.com>
2021-04-28 12:23:49 +0000

    Add project files.

---
+++ MBExtensions/Extensions/Files/FormFileExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBExtensions.Extensions.Files
+{
+    public static class FormFileExtensions
+    {
+        /// <summary>
+        /// Transfer an <see cref="IFormFile"/> to an array of bytes
+        /// </summary>
+        /// <param name="file">The <see cref="IFormFile"/> to be transferred</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
+        public static async Task<byte[]> ToBytesAsync( this IFormFile file )
+        {
+            var stream = new MemoryStream();
+
+            await file.CopyToAsync( stream );
+
+            return stream.ToArray();
+        }
+    }
+}
