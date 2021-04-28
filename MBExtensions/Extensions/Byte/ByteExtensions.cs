9612b3ca3220591542cff78353ecb92934c1c1f4
MBARK T3STO <mbarktiesto@outlook.com>
2021-04-28 18:20:30 +0000

    ByteExtensions added

---
+++ MBExtensions/Extensions/Byte/ByteExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MBExtensions.Extensions.Byte
+{
+    public static class ByteExtensions
+    {
+        /// <summary>
+        /// Convrt a <see cref="byte"/> array to <b>base64</b> as <see cref="string"/> format
+        /// </summary>
+        /// <param name="bytes">Bytes to be converted</param>
+        /// <returns><see cref="Task{TResult}"/></returns>
+        public static Task<string> ToBase64Async( this byte[] bytes ) => Task.Factory.StartNew( () => Convert.ToBase64String( bytes ) );
+    }
+}
