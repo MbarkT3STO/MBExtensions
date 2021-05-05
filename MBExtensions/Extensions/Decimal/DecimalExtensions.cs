120ade6b8872d6ffe5d0eb563a38d975f78350f9
MBARK T3STO <mbarktiesto@outlook.com>
2021-05-05 03:28:50 +0000

    DecimalExtensions and SupportedCurrency Added

---
+++ MBExtensions/Extensions/Decimal/DecimalExtensions.cs
@@ -0,0 +1,28 @@
+﻿using MBExtensions.Extensions.Decimal.Enums;
+
+namespace MBExtensions.Extensions.Decimal
+{
+    public static class DecimalExtensions
+    {
+        /// <summary>
+        /// Convert a <see cref="decimal"/> to <see cref="string"/> currency format
+        /// </summary>
+        /// <param name="value">Value to be converted</param>
+        /// <returns><see cref="string"/></returns>
+        public static string ToStringCurrencyFormat( this decimal value )
+        {
+            return value.ToString( "C" );
+        } 
+        
+        /// <summary>
+        /// Convert a <see cref="decimal"/> to <see cref="string"/> currency format
+        /// </summary>
+        /// <param name="value">Value to be converted</param>
+        /// <param name="currency"><see cref="SupportedCurrency"/> as abbreviation</param>
+        /// <returns><see cref="string"/></returns>
+        public static string ToStringCurrencyFormat( this decimal value,SupportedCurrency currency )
+        {
+            return value.ToString( "00.00 " + currency.ToString() );
+        }
+    }
+}
