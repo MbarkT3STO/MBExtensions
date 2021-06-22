c93116267838cfebe2812f7be3e6742a1f4f281a
MBARK T3STO <mbarktiesto@outlook.com>
2021-06-22 17:32:36 +0100

    EnumExtensions => GetDisplayName added

---
+++ MBExtensions/Extensions/Enums/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MBExtensions.Extensions.Enums
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Get a <see cref="DisplayAttribute"/> Name's for an <see cref="Enum"/>
+        /// </summary>
+        /// <param name="enumValue">The enum value to get Display Name for</param>
+        /// <returns><see cref="string"/></returns>
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType().GetMember(enumValue.ToString()).First().GetCustomAttribute<DisplayAttribute>()?.GetName();
+        }
+    }
+}
