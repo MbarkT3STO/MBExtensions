0daa3725b148d96e8d45f6771efd5e5cdbfb2171
MBARK T3STO <mbarktiesto@outlook.com>
2021-08-20 14:46:07 +0100

    Added => GetExtension : Method

--- MBExtensions/Extensions/Files/FormFileExtensions.cs
+++ MBExtensions/Extensions/Files/FormFileExtensions.cs
@@ -39,5 +39,37 @@ namespace MBExtensions.Extensions.Files
         /// <param name="file">File to be checked</param>
         /// <returns><see cref="bool"/></returns>
         public static bool IsNotNull( this IFormFile file ) => file != null;
+
+        /// <summary>
+        /// Asynchronously save an <see cref="IFormFile"/> object as a physical file
+        /// </summary>
+        /// <param name="file"><see cref="IFormFile"/> object to be saved</param>
+        /// <param name="pathToSaveTo">Path to save to</param>
+        /// <returns><see cref="Task"/></returns>
+        public static Task SaveToAsync(this IFormFile file, string pathToSaveTo)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                using (Stream fileStream = File.Open(pathToSaveTo, FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+                {
+                    file.CopyTo(fileStream);
+                }
+
+                //using (Stream fileStream = new FileStream(pathToSaveTo, FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+                //{
+                //    file.CopyTo(fileStream);
+                //}
+            });
+        }
+
+        /// <summary>
+        /// Get the extension of the current <see cref="IFormFile"/>
+        /// </summary>
+        /// <param name="file"><see cref="IFormFile"/> object to get the extension from</param>
+        /// <returns><see cref="string"/></returns>
+        public static string GetExtension(this IFormFile file)
+        {
+            return Path.GetExtension( file.FileName );
+        }
     }
 }
