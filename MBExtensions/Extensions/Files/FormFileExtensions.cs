00d1e913d3045f62d5903d9756e4ba23c8c8dec1
MBARK T3STO <mbarktiesto@outlook.com>
2021-08-22 17:25:35 +0100

    Added => ToMBXelCoreWorkbookAsync : File Extension

--- MBExtensions/Extensions/Files/FormFileExtensions.cs
+++ MBExtensions/Extensions/Files/FormFileExtensions.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using MBXel_Core.Core.Units;
+using MBXel_Core.Enums;
+using Spire.Xls;
+using Workbook = MBXel_Core.Core.Workbook;
 
 namespace MBExtensions.Extensions.Files
 {
@@ -26,6 +30,7 @@ namespace MBExtensions.Extensions.Files
             return stream.ToArray();
         }
 
+
         /// <summary>
         /// Check if an <see cref="IFormFile"/> has no content
         /// </summary>
@@ -39,6 +44,7 @@ namespace MBExtensions.Extensions.Files
         /// <param name="file">File to be checked</param>
         /// <returns><see cref="bool"/></returns>
         public static bool IsNotNull( this IFormFile file ) => file != null;
+
 
         /// <summary>
         /// Asynchronously save an <see cref="IFormFile"/> object as a physical file
@@ -70,6 +76,56 @@ namespace MBExtensions.Extensions.Files
         public static string GetExtension(this IFormFile file)
         {
             return Path.GetExtension( file.FileName );
+        }
+
+
+        /// <summary>
+        /// Get an <see cref="IFormFile"/> as an MBXelCore workbook
+        /// </summary>
+        /// <param name="file"><see cref="IFormFile"/> to be converted to MBXelCore <see cref="Workbook"/></param>
+        /// <param name="extension">File extension</param>
+        /// <param name="version">File version</param>
+        /// <param name="password">Open password if the <see cref="IFormFile"/> workbook need</param>
+        /// <returns></returns>
+        public static Task<Workbook> ToMBXelCoreWorkbookAsync(this IFormFile file, XLExtension extension=XLExtension.Xlsx, ExcelVersion version= ExcelVersion.Version2016, string password=null)
+        {
+            return Task.Run( async () =>
+                             {
+                                 await using var ms = new MemoryStream();
+                                 await file.CopyToAsync( ms );
+
+                                 var workBookConfig = new WorkbookConfig()
+                                                      {
+                                                          Path      = file.Name ,
+                                                          Password  = password ,
+                                                          Extension = extension ,
+                                                          Version   = version
+                                                      };
+                                 var workBook = new Workbook( workBookConfig );
+
+                                 workBook.LoadFromStream( ms );
+
+                                 return workBook;
+                             } );
+        }
+        
+        /// <summary>
+        /// Get an <see cref="IFormFile"/> as an MBXelCore workbook
+        /// </summary>
+        /// <param name="file"><see cref="IFormFile"/> to be converted to MBXelCore <see cref="Workbook"/></param>
+        /// <param name="config">The workbook configurations as an <see cref="WorkbookConfig"/> object</param>
+        public static Task<Workbook> ToMBXelCoreWorkbookAsync(this IFormFile file, WorkbookConfig config)
+        {
+            return Task.Run( async () =>
+                             {
+                                 await using var ms = new MemoryStream();
+                                 await file.CopyToAsync( ms );
+
+                                 var workBook = new Workbook( config );
+                                 workBook.LoadFromStream( ms );
+
+                                 return workBook;
+                             } );
         }
     }
 }
