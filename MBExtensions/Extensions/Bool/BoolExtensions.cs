2a098ebf7b80b1392ab9c18393db58fc8061fd4f
MBARK T3STO <mbarktiesto@outlook.com>
2022-01-06 11:36:21 +0100

    Added => ToYesNo & ToOuiNon : Methods

--- MBExtensions/Extensions/Bool/BoolExtensions.cs
+++ MBExtensions/Extensions/Bool/BoolExtensions.cs
@@ -10,5 +10,19 @@
         /// <param name="valueIfFalse">Value if the <b>boolean</b> is <b>False</b></param>
         /// <returns></returns>
         public static string ToString( this bool booleanValue , string valueIfTrue , string valueIfFalse ) => booleanValue ? valueIfTrue : valueIfFalse;
+
+        /// <summary>
+        /// Return a YES if a boolean is <b>True</b> or NO if <b>False</b> 
+        /// </summary>
+        /// <param name="booleanValue">Boolean value to be checked</param>
+        /// <returns></returns>
+        public static string ToYesNo( this bool booleanValue ) => booleanValue ? "Yes" : "No";
+
+        /// <summary>
+        /// Return a OUI if a boolean is <b>True</b> or NON if <b>False</b> 
+        /// </summary>
+        /// <param name="booleanValue">Boolean value to be checked</param>
+        /// <returns></returns>
+        public static string ToOuiNon( this bool booleanValue ) => booleanValue ? "Oui" : "Non";
     }
 }
