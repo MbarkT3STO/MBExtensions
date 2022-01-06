34eaa20ebf2149dea0a10f17ac491e29d5029d88
MBARK T3STO <mbarktiesto@outlook.com>
2022-01-06 11:24:44 +0100

    Added => ToString : Method

---
+++ MBExtensions/Extensions/Bool/BoolExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace MBExtensions.Extensions.Bool
+{
+    public static class BoolExtensions
+    {
+        /// <summary>
+        /// Return a speific string value if a boolean is <b>True</b> or <b>False</b> 
+        /// </summary>
+        /// <param name="booleanValue">Boolean value to be checked</param>
+        /// <param name="valueIfTrue">Value if the <b>boolean</b> is <b>True</b></param>
+        /// <param name="valueIfFalse">Value if the <b>boolean</b> is <b>False</b></param>
+        /// <returns></returns>
+        public static string ToString( this bool booleanValue , string valueIfTrue , string valueIfFalse ) => booleanValue ? valueIfTrue : valueIfFalse;
+    }
+}
