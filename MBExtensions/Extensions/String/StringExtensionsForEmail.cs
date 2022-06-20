fb5fce3c3802635f24b4d7036c39683b76e7fcd1
MBARK T3STO <mbarktiesto@outlook.com>
2022-06-20 19:54:13 +0100

    Feat: Create Extension method to check iif a string is a valid email address or not

---
+++ MBExtensions/Extensions/String/StringExtensionsForEmail.cs
@@ -0,0 +1,21 @@
+﻿namespace MBExtensions.Extensions.String
+{
+    public class StringExtensionsForEmail
+    {
+        /// <summary>
+        /// Checks if a string is a valid email
+        /// </summary>
+        /// <param name="value">Text to be checked</param>
+        public static bool IsValidEmailAddress(this string value)
+        {
+            var trimmedValue = value.Trim();
+            const string pattern = @"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|"
+                                   + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)"
+                                   + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$";
+
+            var regex = new Regex(pattern, RegexOptions.IgnoreCase);
+
+            return regex.IsMatch(trimmedValue);
+        }
+    }
+}
